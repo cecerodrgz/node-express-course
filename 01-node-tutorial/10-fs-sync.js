ca7dd12417c358acd2ceb472076a4c506ec28a14
Cecilia Rodriguez <cecerodrgz@Cecilias-Air.lan>
2023-10-11 13:23:23 -0400

    fixed remaining files

--- 01-node-tutorial/10-fs-sync.js
+++ 01-node-tutorial/10-fs-sync.js
@@ -2,10 +2,11 @@ const { readFileSync, writeFileSync, appendFile } = require('fs')
 console.log('start')
 const first = readFileSync('./content/first.txt', 'utf8')
 const second = readFileSync('./content/second.txt', 'utf8')
+const third = readFileSync('./content/second.txt', 'utf8') 
 
 writeFileSync(
   './content/result-sync.txt',
-  `Here is the result : ${first}, ${second}`,
+  `Here is the result : ${first}, ${second}, ${third}`,
   { flag: 'a' }
 )
 
