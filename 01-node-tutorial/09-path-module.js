d993abdeaf1c5093dd7dbcb2fb97b55a127ac519
Cecilia Rodriguez <cecerodrgz@Cecilias-Air.lan>
2023-10-10 20:04:48 -0400

    fixed files;

--- 01-node-tutorial/09-path-module.js
+++ 01-node-tutorial/09-path-module.js
@@ -8,5 +8,5 @@ console.log(filePath)
 const base = path.basename(filePath)
 console.log(base)
 
-const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt')
+const absolute = path.join(__dirname, 'content', 'subfolder', 'test.txt')
 console.log(absolute)
